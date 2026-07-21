cd51c2a9f765dcf3eb0677f6929daf270ea75e1a
PlumeIS <3172754943@qq.com>
2026-07-21 17:04:46 +0800

    [feature] 阵营管理接口

--- src/main/java/cn/plumc/camp/camp/InviteRule.java
+++ src/main/java/cn/plumc/camp/camp/InviteRule.java
@@ -1,4 +1,52 @@
 package cn.plumc.camp.camp;
 
-public class InviteRule {
+import org.bukkit.scoreboard.Team;
+
+import javax.annotation.Nullable;
+
+public enum InviteRule {
+    DEFAULT("camp.invite_rule.default", "默认/申请"),
+    ACCEPT_ALL("camp.invite_rule.accept_all", "全部接受"),
+    ACCEPT_KEY("camp.invite_rule.accept_key", "填写密钥");
+    public final String key;
+    public final String info;
+    InviteRule(String key, String info) {
+        this.key = key;
+        this.info = info;
+    }
+    public static InviteRule get(Team team){
+        for (String entry : team.getEntries()) {
+            if (entry.equals(ACCEPT_ALL.key)) return ACCEPT_ALL;
+            if (entry.startsWith(ACCEPT_ALL.key)) return ACCEPT_KEY;
+        }
+        return DEFAULT;
+    }
+
+    public static void set(Team team, InviteRule inviteRule, @Nullable String value){
+        clearRule(team);
+        switch (inviteRule) {
+            case DEFAULT -> team.addEntry(DEFAULT.key);
+            case ACCEPT_ALL -> team.addEntry(ACCEPT_ALL.key);
+            case ACCEPT_KEY -> team.addEntry(ACCEPT_KEY.key+"."+value);
+        }
+    }
+
+    public static void clearRule(Team team) {
+        for (String entry : team.getEntries()) {
+            if (entry.startsWith(ACCEPT_KEY.key)) team.removeEntry(entry);
+            if (entry.equals(ACCEPT_ALL.key)) team.removeEntry(entry);
+            if (entry.equals(DEFAULT.key)) team.removeEntry(entry);
+        }
+    }
+
+    public static String getAcceptKey(Team team) {
+        for (String entry : team.getEntries()) {
+            if (entry.startsWith(ACCEPT_KEY.key)) return entry.substring(ACCEPT_KEY.key.length()+1);
+        }
+        return null;
+    }
+
+    public static boolean isInviteRule(String key) {
+        return key.startsWith("camp.invite_rule.");
+    }
 }
--- src/main/java/cn/plumc/camp/commands/CampCommand.java
+++ src/main/java/cn/plumc/camp/commands/CampCommand.java
@@ -1,4 +1,49 @@
 package cn.plumc.camp.commands;
 
-public class CampCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CampCommand implements TabCompleter, CommandExecutor {
+    enum SubCommands{
+        CREATE("create"),
+        DISBAND("disband"),
+        LIST("list"),
+        AUTH("auth"),
+        JOIN("join"),
+        LEAVE("leave"),
+        MODIFY("modify"),
+        MEMBER("member");
+        final String command;
+        SubCommands(String command) {
+            this.command = command;
+        }
+        public SubCommands fromString(String command) {
+            for (SubCommands subCommand : values()) {
+                if (subCommand.command.equalsIgnoreCase(command)) {
+                    return subCommand;
+                }
+            }
+            return null;
+        }
+        public List<String> all(){
+            return Arrays.stream(values()).map(c -> c.command).toList();
+        }
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return List.of();
+    }
 }
